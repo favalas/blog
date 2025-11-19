6cb50152ded26fb0f908186671b60a9cf5185e3a
favalas <favalas@pm.me>
2025-11-19 14:32:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ function JumpToEndOfPage()
     { left: 0, top: document.body.scrollHeight || document.documentElement.scrollHeight, behavior: "smooth" }
   );   
 };
+
+function JumpToTopOfPage()
+{  
+  window.scrollTo(
+    { left: 0, top: 0, behavior: "smooth" }
+  );   
+};
