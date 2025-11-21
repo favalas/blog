1054a6d40cc9e3a8e61d4958ff94048833071b6d
favalas <favalas@pm.me>
2025-11-21 15:04:37 +0100

    Create inverted_index_job.php to aggregate HTML content
    This script aggregates the body text from multiple HTML files into a single string.

---
+++ cronjob/inverted_index_job.php
@@ -0,0 +1,25 @@
+<?php
+
+use DOM\HTMLDocument;
+
+$file_root = "../";
+
+$files = array("quotes.html", 
+               "notesfromtheundergroundp1.html", 
+               "notesfromtheundergroundp2.html",
+               "notesfromtheundergroundp3.html",
+               "notesfromtheundergroundp4.html");
+
+$total_files_content = "";
+
+foreach($files as $file) {
+    $dom = HTMLDocument::createFromFile($file_root . $file);
+
+    $body_text = $dom->getElementsByTagName('body')->item(0)->$textContent;
+
+    $total_files_content .= $body_text;
+}
+
+echo $body_text;
+
+?>
