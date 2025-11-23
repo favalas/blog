484db6765be82c706f17da83d778cd95fcbdc062
favalas <favalas@pm.me>
2025-11-23 16:58:44 +0100

    Update inverted_index_job.php

--- cronjob/inverted_index_job.php
+++ cronjob/inverted_index_job.php
@@ -34,6 +34,7 @@ foreach($files as $file) {
                  '/\[/', 
                  '/\]/',
                  '/\’s/',
+                 '/\'s/',
                  '/\”/');
       $term_parsed = preg_replace($patterns, "", $term);
       
